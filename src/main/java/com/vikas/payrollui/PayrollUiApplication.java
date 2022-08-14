be7f8c6667db568d62d52c0f66018a66cc174d76
vikas <vikas134rana@gmail.com>
2022-08-14 17:40:21 +0530

    Initial commit

---
+++ src/main/java/com/vikas/payrollui/PayrollUiApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.payrollui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikas/payrollui/PayrollUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.payrollui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
